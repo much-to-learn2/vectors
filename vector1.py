14d9528bd420b1d4154a3e2c95a6ebfbea3a5d84
much-to-learn2 <mattdgraham95@gmail.com>
2019-08-24 13:33:21 -0400

    add vector1.py

---
+++ vector1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"""
+Version 1 of n-dimensional vector, an upgrade from Vector2d
+"""
+
+from array import array
+import reprlib
+import math
+
+
+class Vector:
+    typecode = 'd'
+
+    def __init__(self, components):
+        self._components = array(self.typecode, components)
+
+    def __iter__(self):
+        # __iter__(iterable) = iterator
+        # __iter__(iterator) = self
+        return iter(self._components)
+
+    def __repr__(self):
+        components = reprlib.repr(self._components)
+        components = components[components.find('['):-1]
+        return 'Vector({})'.format(components)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) + 
+                bytes(self._components))
+
+    def __abs__(self):
+        return math.sqrt(sum(x * x for x in self))
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(memv)
